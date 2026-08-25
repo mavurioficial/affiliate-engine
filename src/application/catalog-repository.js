be1eddab0bb371bde83657cf97dba9f93de7fece
mavurioficial <mavuri.oficial@gmail.com>
2026-08-25 15:29:23 -0300

    Expand catalog repository contract for CRUD

--- src/application/catalog-repository.js
+++ src/application/catalog-repository.js
@@ -1,10 +1,10 @@
 /**
- * Contrato de leitura independente do armazenamento.
+ * Contrato de catálogo independente do armazenamento.
  * Implementações futuras podem consultar uma API ou banco sem mudar a interface.
  */
 export class CatalogRepository {
-  list() {
-    throw new Error('CatalogRepository.list() deve ser implementado pelo adaptador.')
-  }
+  list() { throw new Error('CatalogRepository.list() deve ser implementado pelo adaptador.') }
+  create() { throw new Error('CatalogRepository.create() deve ser implementado pelo adaptador.') }
+  update() { throw new Error('CatalogRepository.update() deve ser implementado pelo adaptador.') }
+  remove() { throw new Error('CatalogRepository.remove() deve ser implementado pelo adaptador.') }
 }
-
