96769def7e975e7efdf1de1fd9f2583ce067895b
mavurioficial <mavuri.oficial@gmail.com>
2026-09-22 16:38:53 -0300

    feat: require affiliate link for automatic delivery

--- src/application/mercadolivre-offer-service.js
+++ src/application/mercadolivre-offer-service.js
@@ -42,6 +42,6 @@ export async function captureMercadoLivreOffer(productUrl, {
       sourceTypeDetail: sourceType === 'api' ? 'mercadolivre_url' : sourceType
     }
   })
-  if (result.created) await enqueueOfferDeliveries(result.offer)
+  if (result.created && resolvedAffiliateUrl) await enqueueOfferDeliveries(result.offer)
   return result.offer
 }
